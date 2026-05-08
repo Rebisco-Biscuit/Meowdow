b58351e5b8fb49b1d1a2e63ba1bcecbbfb5fbdbf
erunzd <23103949@usc.edu.ph>
2026-05-08 14:09:58 +0800

    hotbar base

--- meowdow/inventory/inventory.gd
+++ meowdow/inventory/inventory.gd
@@ -34,6 +34,8 @@ func removeSlot(inventorySlot: InventorySlot):
 	if index < 0: return
 	
 	slots[index] = InventorySlot.new()
+	updated.emit()
 
 func insertSlot(index: int, inventorySlot: InventorySlot):
 	slots[index] = inventorySlot
+	updated.emit()
