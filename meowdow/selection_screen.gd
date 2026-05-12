f786bdc456c8c9a821f769bbcaecff02dd9e1c69
erunzd <23103949@usc.edu.ph>
2026-05-12 16:56:16 +0800

    updated selection screen

--- meowdow/selection_screen.gd
+++ meowdow/selection_screen.gd
@@ -38,3 +38,7 @@ func _on_button_pressed() -> void:
 		return
 
 	get_tree().change_scene_to_file("res://Vinalore.tscn")
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
